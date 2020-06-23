ca8aea76f5d6b3c062f12e1cbe07974ac1b15cde
alexander-braun <alexanderbraun@freenet.de>
2020-06-23 22:29:12 +0200

    added spiral traverse array method

---
+++ Algoexpert/SpiralTraverseArray.js
@@ -0,0 +1,66 @@
+//Algo Solution
+function spiralTraverse(array) {
+  let result = [];
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+  let startRow = 0;
+  let endRow = array.length - 1;
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) break;
+      result.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      if (startCol === endCol) break;
+      result.push(array[row][startCol]);
+    }
+
+    startCol++;
+    endCol--;
+    startRow++;
+    endRow--;
+  }
+
+  return result;
+}
+
+//Algo 2 recursive
+function spiralTraverse(array) {
+  let result = [];
+  spiralFill(array, 0, array.length - 1, 0, array[0].length - 1, result);
+  return result;
+}
+
+function spiralFill(array, startRow, endRow, startCol, endCol, result) {
+  if (startRow > endRow || startCol > endCol) return;
+
+  for (let col = startCol; col <= endCol; col++) {
+    result.push(array[startRow][col]);
+  }
+
+  for (let row = startRow + 1; row <= endRow; row++) {
+    result.push(array[row][endCol]);
+  }
+
+  for (let col = endCol - 1; col >= startCol; col--) {
+    if (startRow === endRow) break;
+    result.push(array[endRow][col]);
+  }
+
+  for (let row = endRow - 1; row >= startRow + 1; row--) {
+    if (startCol === endCol) break;
+    result.push(array[row][startCol]);
+  }
+
+  spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result);
+}
