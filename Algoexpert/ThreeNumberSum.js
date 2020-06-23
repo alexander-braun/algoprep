d6420220785c9208ff19e368381d5fadf0cf1968
alexander-braun <alexanderbraun@freenet.de>
2020-06-23 18:50:13 +0200

    added three number sum

---
+++ Algoexpert/ThreeNumberSum.js
@@ -0,0 +1,23 @@
+//Algosolution O(N²) Time / O(N) space
+function threeNumberSum(array, targetSum) {
+  array = array.sort((a, b) => a - b);
+  let triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      let currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else if (currentSum > targetSum) {
+        right--;
+      }
+    }
+  }
+
+  return triplets;
+}
