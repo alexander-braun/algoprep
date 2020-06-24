e54e276fac8ca2ebfeb181f257e1747b8dee5453
alexander-braun <alexanderbraun@freenet.de>
2020-06-24 13:00:44 +0200

    added bst validation

---
+++ Algoexpert/ValidateBST.js
@@ -0,0 +1,11 @@
+function validateBst(tree) {
+  // Write your code here.
+  return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+function validateBstHelper(tree, min, max) {
+  if (tree === null) return true;
+  if (tree.value < min || tree.value >= max) return false;
+  let leftValid = validateBstHelper(tree.left, min, tree.value);
+  return leftValid && validateBstHelper(tree.right, tree.value, max);
+}
