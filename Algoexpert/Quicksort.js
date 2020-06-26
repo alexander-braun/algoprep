649949ea4e49ae7ece405dffcd31edd579c1da49
alexander-braun <alexanderbraun@freenet.de>
2020-06-26 18:40:34 +0200

    added quicksort

--- Algoexpert/Quicksort.js
+++ Algoexpert/Quicksort.js
@@ -6,6 +6,46 @@
 // After the loop the pivot is in the correct position
 // Reapply the technique
 
+// O(Nlog(N)) time
+// O(log(N)) space
+
+function quickSort(array) {
+  quickSortHelper(array, 0, array.length - 1);
+  return array;
+}
+
+function quickSortHelper(array, startIdx, endIdx) {
+  if (startIdx >= endIdx) return;
+  let pivotIdx = startIdx;
+  let leftIdx = pivotIdx + 1;
+  let rightIdx = endIdx;
+
+  while (rightIdx >= leftIdx) {
+    if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
+      let temp = array[rightIdx];
+      array[rightIdx] = array[leftIdx];
+      array[leftIdx] = temp;
+    }
+    if (array[leftIdx] <= array[pivotIdx]) leftIdx++;
+    if (array[rightIdx] >= array[pivotIdx]) rightIdx--;
+  }
+  swap(pivotIdx, rightIdx, array);
+  let leftSubarraySmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
+  if (leftSubarraySmaller) {
+    quickSortHelper(array, startIdx, rightIdx - 1);
+    quickSortHelper(array, rightIdx + 1, endIdx);
+  } else {
+    quickSortHelper(array, rightIdx + 1, endIdx);
+    quickSortHelper(array, startIdx, rightIdx - 1);
+  }
+}
+
+function swap(i, j, array) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
 // [8, 5, 2, 9, 5, 6, 3] P = 8
 //  P  L              R
 // [8, 5, 2, 9, 5, 6, 3] -> left not bigger then eight shift left pointer by one
